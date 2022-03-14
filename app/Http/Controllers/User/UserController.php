7f6be5726c657ed7efb57d60e9da757fb6296f37
Mayuresh Gramopadhye <mayuresh.gramopadhye@tech9.com>
2022-03-14 19:15:06 +0530

    basic changes

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -36,4 +36,33 @@ class UserController extends Controller
         return response('Hello World', 200)
         ->header('Content-Type', 'text/plain');
     }
+
+    /**
+     * @OA\Get(
+     *      path="/api/user/test",
+     *      operationId="testFunction",
+     *      tags={"user"},
+     *      summary="test summary",
+     *      description="This is a test description",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
+
+    public function test()
+    {
+        return response('Hello World!!!', 200)
+            ->header('Content-Type', 'text/plain');
+    }
 }
--- routes/api/user/user.php
+++ routes/api/user/user.php
@@ -3,4 +3,5 @@ use App\Http\Controllers\User\UserController;
 
 Route::middleware('auth:users')->group(function () {
     Route::get('demo', [UserController::class, 'demo']);
+    Route::get('test', [UserController::class, 'test']);
 });
