e9cfcbd76e51d596aac2c190fbaf5a7c7c1d411f
darshanamunde-tamayou <darshana.munde@tamayou.com>
2022-04-10 11:57:33 +0530

    Changes for social login

--- config/services.php
+++ config/services.php
@@ -32,6 +32,6 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'FACEBOOK_REDIRECT_URI',
+        'redirect' => env('FACEBOOK_REDIRECT_URI'),
     ],
 ];
